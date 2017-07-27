3792629ab56476bef302e1f342472f2013658756
pulkitrathi17 <pulkitrathi98@gmail.com>
2017-07-27 19:51:42 +0530

    phone call

--- app/src/main/java/com/example/rajk/leasingmanagers/employee/Emp_details.java
+++ app/src/main/java/com/example/rajk/leasingmanagers/employee/Emp_details.java
@@ -3,6 +3,7 @@ package com.example.rajk.leasingmanagers.employee;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -128,6 +129,10 @@ public class Emp_details extends AppCompatActivity implements EmployeeTask_Adapt
             @Override
             public void onClick(View v) {
                 //TODO phone call
+
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:"+ num));
+                startActivity(callIntent);
             }
         });
     }
