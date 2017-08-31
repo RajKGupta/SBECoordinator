6c65e7e00086c4cb9da0348136a216afc443d9d1
SoumyaAgarwal <soumya.agarwal@gmail.com>
2017-08-31 20:31:03 +0530

    hh

--- app/src/main/java/com/example/rajk/leasingmanagers/services/DownloadFileForChatService.java
+++ app/src/main/java/com/example/rajk/leasingmanagers/services/DownloadFileForChatService.java
@@ -10,12 +10,14 @@ import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
@@ -52,55 +54,76 @@ public class DownloadFileForChatService extends IntentService {
         super.onDestroy();
     }
 
-    public void downloadFile(final String url, final String type, final String dbTableKey)
-    {
+    public void downloadFile(final String url, final String type, final String dbTableKey) {
         final DatabaseReference[] dbChat = {DBREF.child("Chats").child(dbTableKey).child("ChatMessages").getRef()};
-        StorageReference str = FirebaseStorage.getInstance().getReferenceFromUrl(url);
+        final StorageReference str = FirebaseStorage.getInstance().getReferenceFromUrl(url);
 
         switch (type) {
             case "photo":
-                File rootPath = new File(Environment.getExternalStorageDirectory(), AppName+"/Images");
-                if (!rootPath.exists()) {
-                    rootPath.mkdirs();
-                }
-                String uriSting = System.currentTimeMillis() + ".jpg";
 
-                final File localFile = new File(rootPath, uriSting);
-                final String localuri = (rootPath.getAbsolutePath() + "/" + uriSting);
-                str.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                final String[] ext = new String[1];
+                str.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
                     @Override
-                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                        Log.e("firebase ", ";local tem file created  created " + localFile.toString());
-                        dbChat[0] = DBREF.child("Chats").child(dbTableKey).child("ChatMessages").getRef();
-                        dbChat[0].child(Id).child("othersenderlocal_storage").setValue(localuri);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        Log.e("firebase ", ";local tem file not created  created " + exception.toString());
+                    public void onSuccess(StorageMetadata storageMetadata) {
+                        ext[0] = storageMetadata.getContentType();
+                        int p = ext[0].lastIndexOf("/");
+                        String l = "." + ext[0].substring(p + 1);
+
+                        File rootPath = new File(Environment.getExternalStorageDirectory(), AppName + "/Images");
+                        if (!rootPath.exists()) {
+                            rootPath.mkdirs();
+                        }
+                        String uriSting = System.currentTimeMillis() + l;
+                        final File localFile = new File(rootPath, uriSting);
+                        final String localuri = (rootPath.getAbsolutePath() + "/" + uriSting);
+                        str.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                Log.e("firebase ", ";local tem file created  created " + localFile.toString());
+                                dbChat[0] = DBREF.child("Chats").child(dbTableKey).child("ChatMessages").getRef();
+                                dbChat[0].child(Id).child("othersenderlocal_storage").setValue(localuri);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                Log.e("firebase ", ";local tem file not created  created " + exception.toString());
+                            }
+                        });
+
                     }
                 });
+
                 break;
             case "doc":
-                rootPath = new File(Environment.getExternalStorageDirectory(), AppName+"/Docs");
-                if (!rootPath.exists()) {
-                    rootPath.mkdirs();
-                }
-                uriSting = System.currentTimeMillis() + ".jpg";
 
-                final File localdocFile = new File(rootPath, uriSting);
-                final String localdocuri = (rootPath.getAbsolutePath() + "/" + uriSting);
-                str.getFile(localdocFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                final String[] ext1 = new String[1];
+                str.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
                     @Override
-                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                        dbChat[0] = DBREF.child("Chats").child(dbTableKey).child("ChatMessages").getRef();
-                        dbChat[0].child(Id).child("othersenderlocal_storage").setValue(localdocuri);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-//                        mAdapter.dismissProgressBar(holder);
-                        Log.e("firebase ", ";local tem file not created  created " + exception.toString());
+                    public void onSuccess(StorageMetadata storageMetadata) {
+                        ext1[0] = storageMetadata.getContentType();
+                        int p = ext1[0].lastIndexOf("/");
+                        String l = "." + ext1[0].substring(p + 1);
+
+                        File rootPath = new File(Environment.getExternalStorageDirectory(), AppName + "/Docs");
+                        if (!rootPath.exists()) {
+                            rootPath.mkdirs();
+                        }
+
+                        String uriSting = System.currentTimeMillis() + l;
+                        final File localdocFile = new File(rootPath, uriSting);
+                        final String localdocuri = (rootPath.getAbsolutePath() + "/" + uriSting);
+                        str.getFile(localdocFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                dbChat[0] = DBREF.child("Chats").child(dbTableKey).child("ChatMessages").getRef();
+                                dbChat[0].child(Id).child("othersenderlocal_storage").setValue(localdocuri);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                Log.e("firebase ", ";local tem file not created  created " + exception.toString());
+                            }
+                        });
                     }
                 });
                 break;
--- app/src/main/java/com/example/rajk/leasingmanagers/services/DownloadFileService.java
+++ app/src/main/java/com/example/rajk/leasingmanagers/services/DownloadFileService.java
@@ -2,10 +2,12 @@ package com.example.rajk.leasingmanagers.services;
 
 import android.app.IntentService;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.NotificationCompat;
@@ -18,6 +20,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
@@ -73,40 +76,67 @@ public class DownloadFileService extends IntentService {
 
     public void downloadFile(final String url, final String task_id) {
 
-        StorageReference mStorageRef = FirebaseStorage.getInstance().getReferenceFromUrl(url);
-        File rootPath = new File(Environment.getExternalStorageDirectory(), AppName +"/Images");
-        if (!rootPath.exists()) {
-            rootPath.mkdirs();
-        }
-        String uriSting = System.currentTimeMillis() + ".jpg";
-
-        final File localFile = new File(rootPath, uriSting);
-
-        mStorageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+        final StorageReference mStorageRef = FirebaseStorage.getInstance().getReferenceFromUrl(url);
+        final String[] ext = new String[1];
+        mStorageRef.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
             @Override
-            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                Log.e("firebase ", ";local tem file created  created " + localFile.toString());
-                Toast.makeText(DownloadFileService.this, "Downloaded Quotation", Toast.LENGTH_SHORT).show();
-                updateNotification("Succesfully Downloaded");
+            public void onSuccess(StorageMetadata storageMetadata) {
+                ext[0] =  storageMetadata.getContentType();
+                int p = ext[0].lastIndexOf("/");
+                String l = "."+ext[0].substring(p + 1);
+                final String open = ext[0].substring(0, p);
 
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                Toast.makeText(DownloadFileService.this, "Download Failed", Toast.LENGTH_SHORT).show();
-                updateNotification("Download failed");
-                Log.e("firebase ", ";local tem file not created  created " + exception.toString());
-            }
-        }).addOnProgressListener(new OnProgressListener<FileDownloadTask.TaskSnapshot>() {
-            @Override
-            public void onProgress(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                File rootPath = new File(Environment.getExternalStorageDirectory(), AppName+"/Docs");
+                if (!rootPath.exists()) {
+                    rootPath.mkdirs();
+                }
 
+                String uriSting = System.currentTimeMillis() + l;
+                final File localFile = new File(rootPath, uriSting);
+
+                mStorageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        Log.e("firebase ", ";local tem file created  created " + localFile.toString());
+                        Toast.makeText(DownloadFileService.this, "Downloaded Quotation", Toast.LENGTH_SHORT).show();
+                        updateNotification("Succesfully Downloaded",open,localFile);
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Toast.makeText(DownloadFileService.this, "Download Failed", Toast.LENGTH_SHORT).show();
+                        updateNotification("Download failed",open,localFile);
+                        Log.e("firebase ", ";local tem file not created  created " + exception.toString());
+                    }
+                }).addOnProgressListener(new OnProgressListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(FileDownloadTask.TaskSnapshot taskSnapshot) {
+
+                    }
+                });
             }
         });
+
+
     }
 
-    private void updateNotification(String information) {
+    private void updateNotification(String information, String open, File localFile) {
         notificationManager.cancel(0);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Toast.makeText(this, open, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        if (localFile.exists()) {
+            Uri pdfPath = Uri.fromFile(localFile);
+            if (open.equals("application")) {
+                intent.setDataAndType(pdfPath, "application/pdf");
+            }
+            else {
+                intent.setDataAndType(pdfPath, "image/*");
+            }
+        }
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
                 getApplicationContext());
         int icon = R.mipmap.ic_launcher;
@@ -118,13 +148,12 @@ public class DownloadFileService extends IntentService {
                 .setColor(getApplicationContext().getResources().getColor(R.color.white))
                 .setContentText(information)
                 .setAutoCancel(true)
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(information));
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(information))
+                .setContentIntent(contentIntent);
 
-        synchronized (this) {
-            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(0, mBuilder.build());
-            stopSelf();
-        }
+        notificationManager.notify(0, mBuilder.build());
+        stopSelf();
+
     }
 
 
