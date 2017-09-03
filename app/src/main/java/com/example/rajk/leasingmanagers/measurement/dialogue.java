885b9d8ea44e67811e0004752b7cbc81a692e3e6
pulkitrathi17 <pulkitrathi98@gmail.com>
2017-09-03 18:34:51 +0530

    measurement

--- app/src/main/java/com/example/rajk/leasingmanagers/measurement/dialogue.java
+++ app/src/main/java/com/example/rajk/leasingmanagers/measurement/dialogue.java
@@ -95,7 +95,7 @@ public class dialogue extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                FilePickerBuilder.getInstance().setMaxCount(10)
+                FilePickerBuilder.getInstance().setMaxCount(1)
                         .setActivityTheme(R.style.AppTheme)
                         .pickPhoto(dialogue.this);    }
         });
